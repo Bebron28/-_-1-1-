b369c68a0eadcdded1bcf2224f7605b0a834b0ce
Bebron28 <basurmanovstepan28@gmail.com>
2025-01-27 21:01:51 +0300

    Add files via upload

---
+++ Модуль_1(1).py
@@ -0,0 +1,25 @@
+#Задание1
+a = int(input("введите число:"))
+b = int(input("введите число:"))
+c = int(input("введите число:"))
+summ = a + b + c
+ymn = a * b * c
+print("Сумма =", summ, "\n", "Произведение =", ymn)
+
+#Задание2
+q = int(input("введите зп за месяц:"))
+w = int(input("введите месяцный платеж в банке:"))
+e = int(input("введите задолженность за комм услуги:"))
+print(q - w - e)
+
+#Задание3
+first_diagonal = int(input("введите первую диагональ:"))
+second_diagonal = int(input("введите вторую диагональ:"))
+s = 0.5 * first_diagonal * second_diagonal
+print("Площадь ромба равна ", s)
+
+#Задание4
+print("                             To be", "\n", "                            or not", "\n", "                            to be")
+
+#Задание5
+print('    "Life is what happens', "\n", "         when", "\n", '            youre busy making other plans"', "\n", "                                       John Lennon.")
